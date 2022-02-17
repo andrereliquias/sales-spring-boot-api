5e208963086ffd284f7f4643cacd1232182945c1
AndreReliquias <andrereliquias.contato@gmail.com>
2022-02-17 13:01:48 -0400

    Endpoint hello

--- src/main/java/br/com/andrereliquias/VendasApplication.java
+++ src/main/java/br/com/andrereliquias/VendasApplication.java
@@ -2,6 +2,8 @@ package br.com.andrereliquias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
@@ -9,11 +11,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@RestController // Essa anotation diz que essa classe vai ser um controlador Rest
 public class VendasApplication 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         SpringApplication.run(VendasApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String helloWorld()  {
+        return "Hello  World!";
     }
 }
