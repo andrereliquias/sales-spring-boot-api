561e7ff6f2aa672fdf847a6cc1cb75eb2ca42be2
AndreReliquias <andrereliquias.contato@gmail.com>
2022-02-17 12:37:34 -0400

    first commit

---
+++ src/main/java/br/com/andrereliquias/App.java
@@ -0,0 +1,13 @@
+package br.com.andrereliquias;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
